c31b749d564f406a33332c4ef5d15ffdf7cd6be1
Eason <ejin458@aucklanduni.ac.nz>
2023-10-02 13:51:38 +1300

    Added new dialogur system

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -5,27 +5,39 @@ using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    public GameObject dialogueObject;
-    public TMP_Text dialgoueText;
-    private string intro1 = "You are a villager of a nearby town.";
-    private string intro2 = "You have a habit of causing trouble.";
-    private string intro3 = "You've been given an allowance of 10 gold. It may come in handy later.";
-    private string introDir = "Woods to the left. Town to the right.";
+    private Queue<string> sentences;
 
     public void Start()
     {
-        dialogueObject.SetActive(true);
-        dialgoueText.text = intro1;
-        float scale = 0.1f;
-        // dialogueObject.transform.localScale = new Vector3(scale, scale, scale);
-        // RectTransform rectTransform = dialogueObject.GetComponent<RectTransform>();
-        // rectTransform.sizeDelta = new Vector3(scale, scale, scale);
-        
+        sentences = new Queue<string>();
     }
 
-    public void Intro2() { dialgoueText.text = intro2; }
-    public void Intro3() { dialgoueText.text = intro3; }
-    public void IntroDir() { dialgoueText.text = introDir; }
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation");
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
 
+        DisplayNextSentence();
+    }
 
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
 }
